dc4debdf6448b39479a3ddcdf9949bd86bd1440e
jjwon915 <jjwon915@naver.com>
2021-04-15 22:32:28 +0900

    sql

---
+++ MVC01/src/kr/bit/model/MemberDAO.java
@@ -0,0 +1,11 @@
+package kr.bit.model;
+
+// JDDBC -> myBatis, JPA로 바뀌면서 많이 사용 X
+import java.sql.*;
+public class MemberDAO {
+	private Connection conn;
+	private PreparedStatement ps;
+	private ResultSet rs;
+	
+	
+}
