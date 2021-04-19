8bc496455c170237e928bd4babf8d5b706c3b14a
jjwon915 <jjwon915@naver.com>
2021-04-19 13:33:26 +0900

    DB를 연결할 때 java에서 제공하는 API의  interface(import java.sql.*;)의 추상메소드들을 가져가다 구현 메소드들을 생성한 구현 클래스를 Driver(com.mysql.jdbc.Driver)라고 한다. 구현클래스들은 DBMS에서 이미 생성해둔 것이다. 따라서 MySQL, Oracle, MS-SQL등 서로 다른 DBMS를 사용해도 java에서 제공하는 interface만 사용하면 편리하게 사용할 수 있다. 자바 개발자들은 interface 사용법만 알면 된다.

--- MVC01/src/kr/bit/model/MemberDAO.java
+++ MVC01/src/kr/bit/model/MemberDAO.java
@@ -7,5 +7,21 @@ public class MemberDAO {
 	private PreparedStatement ps;
 	private ResultSet rs;
 	
+	// DB 연결객체 생성(TEST DB 접속 URL, user, password 필요)
+	public void getConnect() {
+		String URL = "jdbc:mysql://localhost:3306/test?characterEncoding=UTF-8&serverTimezone=UTC";
+		String user = "root";
+		String password ="1234";
+		
+		// MySQL Driver Memory에 Loading
+		try {
+			// 동적 로딩(실행 시점에 메모리에 로딩)
+			Class.forName("com.mysql.jdbc.Driver"); 
+			// MySQL DB 접속 시도 후 성공시 연결 정보를 Connection 객체인 conn에 넘겨준다. 실패시 예외 발생.
+			conn = DriverManager.getConnection(URL, user, password); 
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
 	
 }
