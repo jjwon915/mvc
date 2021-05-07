d3fd0d9b22089a18177454f48bc812adc74b2d08
jjwon915 <jjwon915@naver.com>
2021-05-07 23:23:31 +0900

    redirect기법으로 View page에 data 보내기 - redirect 기법으로 보낼 때는 새로 연결이 되기 때문에 get방식으로 붙여서 보내야 한다. 따라서 view page에서는 getParameter()를 통해 data를 받아올 수 있다. - 이 방법은 데이터가 많아지게 되면 매우 불편해지는 문제가 발생한다. - 이 방법을 해결하기 위해서는 객체 바인딩을 사용해야 한다.

--- MVC03/src/kr/bit/forward/ForwardController.java
+++ MVC03/src/kr/bit/forward/ForwardController.java
@@ -11,8 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 public class ForwardController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		// 1. forward 실습
-		String data = "apple";
+		String name = "jewon";
+		int age = 25;
+		String email = "admin@gmail.com";
+		// View Page(result.jsp)로 데이터를 전달하여 View Page에서 출력.
+		// controller에서 View로 페이지를 전환하는 방법 두 가지 => 1. redirect 2. forward
+		// 1. redirect
+		// request.setAttribute("data", data);
+		response.sendRedirect("view/result.jsp?name="+name+"&age="+age+"&email="+email);
 	}
 
 }
