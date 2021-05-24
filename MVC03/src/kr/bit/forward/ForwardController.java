5f85b2b0ab57334a4f01ba07e940e42b07df9079
jjwon915 <jjwon915@naver.com>
2021-05-24 15:51:46 +0900

    객체 바인딩을 통해 Controller에서 View로 데이터를 전달한다. MemberVO 객체를 만들어 SetAttribute해서 View인 forward.jsp로 전달한다. 해당방식은 쿼리스트링방식에비해 보안과 전달 측면에서 나아졌다고 말할 수 있다. 객체를 사용해 전달하기 때문에 들어있는 데이터를 노출시키지 않아도 된다.

--- MVC03/src/kr/bit/forward/ForwardController.java
+++ MVC03/src/kr/bit/forward/ForwardController.java
@@ -1,24 +1,36 @@
 package kr.bit.forward;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.bit.model.MemberVO;
+
 @WebServlet("/fc.do")
 public class ForwardController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		String name = "jewon";
 		int age = 25;
 		String email = "admin@gmail.com";
-		// View Page(result.jsp)로 데이터를 전달하여 View Page에서 출력.
-		// controller에서 View로 페이지를 전환하는 방법 두 가지 => 1. redirect 2. forward
-		// 1. redirect
-		// request.setAttribute("data", data);
-		response.sendRedirect("view/result.jsp?name="+name+"&age="+age+"&email="+email);
+		
+		//forward.jsp :특정 메모리에 setAttribute하고 JSP에서 빼내올 수 있도록 객체 바인딩 한다. 
+		// request에 vo 객체를 넣는다.
+		
+		MemberVO vo = new MemberVO();
+		vo.setName(name);
+		vo.setAge(age);
+		vo.setEmail(email);
+		
+		//객체 바인딩
+		request.setAttribute("vo", vo);
+		
+		//forward
+		RequestDispatcher rd = request.getRequestDispatcher("view/forward.jsp");
+		rd.forward(request, response);
 	}
-
 }
