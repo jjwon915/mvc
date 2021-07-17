f4519baaba3b7a9a0e5cbb28dbdcd2f9a85d8b31
jjwon915 <jjwon915@naver.com>
2021-07-17 19:13:09 +0900

    MemberDAO는 Mapper파일에 담긴 쿼리를 호출해 사용한다. - insert, update, delete와 같은 기능의 경우에는 DB의 내용이 수정되기 때문에 session.commit()을 해준다. - 하나의 기능을 끝낸 후에는 session.close()를 해줘야 Connection POOL로 반납된다. - Mapper 파일의 위치 정보를 쓰지않아도, sqlSessionFactory가 생성될 때 config.xml 파일을 읽기때문에 Mapper파일이 어디에 있는지 모두 알고있다.

--- MVC05/src/kr/bit/model/MemberDAO.java
+++ MVC05/src/kr/bit/model/MemberDAO.java
@@ -4,8 +4,10 @@ import java.io.InputStream;
 // JDBC->myBatis, JPA
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
@@ -16,6 +18,8 @@ public class MemberDAO {
 	// 초기화 블럭 - 프로그램 실행 시 딱 한번만 실행되는 코드 영역
 	static {
 		try {
+		// sqlSessionFactory는 config.xml에 담긴 정보들을 모두 알고있다. 
+		// 따라서 Mapper파일이 어디에 있는지 또한 알고있다.
 		String resource = "kr/bit/mybatis/config.xml";
 		InputStream inputStream;
 		inputStream = Resources.getResourceAsStream(resource);
@@ -23,6 +27,25 @@ public class MemberDAO {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	// 회원 전제 리스트 보기
+	public List<MemberVO> memberList() {
+		// [Connection + Statement] => SQLSession
+		SqlSession  session = sqlSessionFactory.openSession();
+		List<MemberVO> list = session.selectList("memberList");
+		// Connection POOL에 반납.
+		session.close();
+		return list;
+	}
+	
+	// 회원가입
+	public int memberInsert(MemberVO vo) {
+		SqlSession session = sqlSessionFactory.openSession();
+		int cnt = session.insert("SQL_ID", vo);
+		session.commit(); // insert는 DB의 내용이 수정되기 때문에 commit을 꼭 해준다.
+		session.close();
+		return cnt;
 	}
   
 }
